bae13d35fb0016f9d306cff7c4280cfca0460c57
Joran Honig <joran.honig@gmail.com>
2018-09-08 20:02:36 +0200

    cleanup solver

--- mythril/analysis/solver.py
+++ mythril/analysis/solver.py
@@ -1,4 +1,4 @@
-from z3 import Solver, simplify, sat, unknown, unsat
+from z3 import Solver, simplify, sat, unknown
 from mythril.exceptions import UnsatError
 import logging
 
@@ -8,7 +8,6 @@ def get_model(constraints):
 
     for constraint in constraints:
         s.add(constraint)
-
     result = s.check()
     if result == sat:
         return s.model()
